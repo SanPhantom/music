6313799c149ffa1a240f0b3c66b0fdf623b50214
SanPhantom <1300690777@qq.com>
2019-08-12 18:07:32 +0800

    music 1.0.0

---
+++ src/components/MusicPlay.vue
@@ -0,0 +1,27 @@
+<template>
+    <mu-paper class="demo-paper" circle :z-depth="1">
+        <mu-circular-progress class="demo-circular-progress" mode="determinate" :value="5.50" color="secondary" :stroke-width="2" :size="64"></mu-circular-progress>
+    </mu-paper>
+</template>
+
+<script>
+    export default {
+        name: 'MusicPlay'
+
+    }
+</script>
+
+<style lang="scss">
+    .demo-paper {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        .demo-circular-progress {
+            transform:rotate(-90deg);
+            -webkit-transform:rotate(-90deg);
+        }
+    }
+</style>
